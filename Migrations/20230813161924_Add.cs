636a14f14ff15a315d8a5a3a9a140a6851f35264
Myroslav <126482778+MyroslavKl@users.noreply.github.com>
2023-08-14 12:02:27 +0300

    Add files via upload

---
+++ Migrations/20230813161924_Add.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskPr.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PesonalInformations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ConfirmPassword = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PesonalInformations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UrlsInfo",
+                columns: table => new
+                {
+                    UrlId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShortUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserModelId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UrlsInfo", x => x.UrlId);
+                    table.ForeignKey(
+                        name: "FK_UrlsInfo_PesonalInformations_UserModelId",
+                        column: x => x.UserModelId,
+                        principalTable: "PesonalInformations",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UrlsInfo_UserModelId",
+                table: "UrlsInfo",
+                column: "UserModelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UrlsInfo");
+
+            migrationBuilder.DropTable(
+                name: "PesonalInformations");
+        }
+    }
+}
