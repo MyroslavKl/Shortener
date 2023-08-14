1c70baf804b9bcece6fad757fab19795af1648fd
Myroslav <126482778+MyroslavKl@users.noreply.github.com>
2023-08-14 11:59:04 +0300

    Add files via upload

---
+++ Data/ApplycationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskPr.Models;
+
+namespace TaskPr.Data
+{
+    public class ApplycationDbContext:DbContext
+    {
+        public ApplycationDbContext(DbContextOptions<ApplycationDbContext> options) : base(options) { }
+
+        public DbSet<UserModel> PesonalInformations { get; set; }
+        public DbSet<UrlModel> UrlsInfo { get; set; }
+    }
+}
